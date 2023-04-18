815df4d7809677a896a6378e2437d34b1804240f
Nathan Neidigh <wildcats6380@gmail.com>
2023-04-18 19:59:58 -0400

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,4 +1,8 @@
-#This is a test file to test the game and its functionalities.
-from BattleSystem import BattleLoader
+# This is a test file to test the game and its functionalities.
+from BattleSystem import BattleSystem
+from Character import *
 
-BattleLoader.load_battle_from_file(0)
+player = Character("Bob", 120, Jobs.WARRIOR, [], {}, {}, is_player=True)
+# BattleSystem.load_battle_from_file(player, 1)
+
+print(Character.load_character_from_file("Goblin"))
