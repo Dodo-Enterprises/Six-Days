232d7be6e50919baeb38b6699defd9e54b6030a4
Jin143 <eugene050704@gmail.com>
2023-05-25 08:08:09 -0400

    Update

--- Test.py
+++ Test.py
@@ -5,4 +5,4 @@ from Character import *
 player = Character("Bob", 120, Jobs.WARRIOR, [], {}, {},
                    arm1=Weapon("Knife (H)", Jobs.WARRIOR, WpnTypes.PIERCE, 80), is_player=True)
 #BattleSystem.load_battle_from_file(player, 1).start()
-BattleSystem.load_battle_from_file(player, 1).start()
+BattleSystem.load_battle_from_file(player, 6).start()
